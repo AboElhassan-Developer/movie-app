9bab5ec123cc1267308b313578fe2f3b31284122
Omar-Ahm-ed <omarahmed2162000@gmail.com>
2025-06-19 23:00:59 +0300

    Add wishlist feature and UI updates

--- src/app/shared/components/movie-card/movie-card.component.ts
+++ src/app/shared/components/movie-card/movie-card.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgClass, CommonModule } from '@angular/common'; // ✅ أضف CommonModule
 
+import { Movie } from '../../../core/services/models/movie.model';
+import { WishlistService } from '../../../core/services/wishlist.service';
 @Component({
   selector: 'app-movie-card',
-  imports: [],
+  standalone: true,
+  imports: [NgClass, CommonModule], // ✅ أضف CommonModule هنا
   templateUrl: './movie-card.component.html',
-  styleUrl: './movie-card.component.scss'
+  styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
+  @Input() movie!: Movie;
+  isInWishlist: boolean = false;
 
+  constructor(private wishlistService: WishlistService) {}
+
+  ngOnInit(): void {
+    this.isInWishlist = this.wishlistService.isInWishlist(this.movie.id);
+  }
+
+  toggleWishlist(): void {
+    if (this.isInWishlist) {
+      this.wishlistService.removeFromWishlist(this.movie.id);
+    } else {
+      this.wishlistService.addToWishlist(this.movie);
+    }
+    this.isInWishlist = !this.isInWishlist;
+  }
 }
